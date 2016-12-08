7517c864384944c73456ee4cbe60b99f6fa4a7d5
Jaime Valdivia <jaivaldivia@gmail.com>
2016-12-07 21:21:08 -0600

    New component

---
+++ src/components/GridComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles//Grid.css');
+
+class GridComponent extends React.Component {
+  render() {
+    return (
+      <div className="grid-component">
+        Please edit src/components///GridComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+GridComponent.displayName = 'GridComponent';
+
+// Uncomment properties you need
+// GridComponent.propTypes = {};
+// GridComponent.defaultProps = {};
+
+export default GridComponent;
